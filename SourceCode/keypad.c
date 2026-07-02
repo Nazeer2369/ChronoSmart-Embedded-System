4f86cf98e93734b59946f1aa8d411d68862ee653
Nazeer2369 <122120157+Nazeer2369@users.noreply.github.com>
2026-07-02 15:02:28 +0530

    Update keypad.c

--- SourceCode/keypad.c
+++ SourceCode/keypad.c
@@ -1,99 +1,3 @@
-/*#include<lpc21xx.h>
-#include"keypad.h"
-#include "lcd.h"
-#define READ_NIBBLE(WORD,POS) ((WORD>>POS)&0XF)
-#define ROW 16
-#define COL 20
-
-int row_no=0,col_no=0;
-//#define READ_BIT(WORD,POS) ((WORD>>POS)&1)
-//port 1
-//16 17 18 19
-
-
-
-//20 21 22 23
-
-unsigned int KEYLUT[4][4]={1 ,2 ,3 ,11,
-													 4 ,5 ,6 ,12,
-							             7 ,8 ,9 ,13,
-							            14 ,0,15 ,16};
-unsigned int arr[4]={14,13,11,7};
-int col_scan()
-{
-	if((READ_NIBBLE(IOPIN1,COL))<15)
-		return 1;
-	return 0;
-}
-int find_row()
-{
-	int i;
-	
-	for(i=0;i<=3;i++)
-	{
-		IOPIN1=(IOPIN1&~(0XF<<ROW))|(arr[i]<<ROW);
-		if(col_scan())
-		{
-			break;
-		}
-		
-		
-	}
-		IOPIN1=(IOPIN1&~(0XF<<ROW))|(0X0<<ROW);
-
-	return i;
-}
-int find_col()
-{
-	int i;
-	for(i=0;i<=3;i++)
-	{
-		if(((IOPIN1>>(COL+i))&1)==0)
-		{
-			break;
-		}
-		
-	}
-	return i;
-}
-	
-void init_keypad()
-{
-	IODIR1 |=0XF<<ROW;
-IODIR1 |=0X0<<COL;
-}
-int key_scan()
-{
-	int row_no,col_no;
-	while(col_scan())
-			{
-				row_no=find_row();
-				
-				col_no=find_col();
-				
-				while(col_scan());
-				//IOPIN0=(IOPIN0&~(0XF<<8))|KEYLUT[row_no][col_no]<<8;
-			}
-			return KEYLUT[row_no][col_no];
-}
-
-int key;
-
-int main()
-{
-	
-	IODIR0|=0XFF<<8;
-	lcd_init();
-	init_keypad();
-	while(1)
-	{
-		key=key_scan();
-			display_int(key);
-	}
-	
-	
-}
-*/
 #include<lpc21xx.h>
 #include"keypad.h"
 #include "lcd.h"
@@ -169,7 +73,6 @@ int key_scan(int *key)
 				col_no=find_col();
 				
 				while(col_scan()==0);
-				//IOPIN0=(IOPIN0&~(0XF<<8))|KEYLUT[row_no][col_no]<<8;
 				*key=KEYLUT[row_no][col_no];
 				return 0;
 			}
